2f9510d6cfd375b325e48d4b093daa72ff01c065
Breno <onerbreno@outlook.com>
2023-02-18 19:38:46 -0300

    html - Aula 233

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -5,7 +5,13 @@ const app = express()
 app.use(urlencoded({extended: true}))
 app.post('/usuarios', (req, res) => {
     console.log(req.body)
-    res.send('Parabéns')
+    // console.log(req.query) no metódo get os dados vem na query string
+    res.send('Parabéns, usuário incluído')
+})
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send('<h1>Parabéns, usuário alterado</h1>')
 })
 
 app.listen(3003)
