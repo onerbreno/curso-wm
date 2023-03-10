0cda984b67c21873c9d4f69c5c8eff9b0373ba13
Breno Dias <onerbreno@outlook.com>
2023-03-10 08:07:47 -0300

    Capítulo de Função - exercício 40

--- js/4_exercicios/10_divisivelPor3.js
+++
@@ -1,8 +0,0 @@
-// 10) Crie uma função que verifica se um número inteiro passado como parêmetro é 
-// divisível por 3 e retorne true ou false.
-
-function verificaDivisivelPor3(num) {
-    return num % 3 === 0
-}
-
-console.log(verificaDivisivelPor3(6))
