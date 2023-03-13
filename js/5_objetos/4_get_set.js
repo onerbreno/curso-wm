7431d61380f014a6f9fb4e6e3cdced90d31e14e5
Breno <onerbreno@outlook.com>
2023-03-12 23:30:04 -0300

    Capítulo de objeto - aula 120

---
+++ js/5_objetos/4_get_set.js
@@ -0,0 +1,45 @@
+const sequencia = {
+    _valor: 1, // o _ é uma convenção para nomes de atributos privados
+    // JS não possui atributos privados, nesse caso é o usado a ideia de closures para "privar" o atributo mas é possível bular isso
+    get valor() { return this._valor++ }, // JS não suporta sobrecarga de métodos, com "exceção" do get e set
+    set valor(valor) {
+        if (valor > this._valor) {
+            this._valor = valor
+        }
+    }
+}
+
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 1000
+console.log(sequencia.valor, sequencia.valor)
+console.log(sequencia["_valor"]) // essa chamada burla os atributos privados
+
+// // GET -> O método get vai buscar o valor de uma propriedade específica
+// let Pessoa1 = {
+//     _valor: "teste", // o "_" é uma convenção conhecida por garantir que outras pessoas que venham trabalhar no código saibam que aquela variável foi pensada para ser privada e não ter seu valor modificado
+//     nome: "João",
+//     sobrenome: "Santos",
+//     idioma: "Português",
+
+//     get linguagem() {
+//         return this.idioma
+//     },
+// }
+// console.log(Pessoa.linguagem)
+// console.log(Pessoa)
+
+
+// // SET -> O set vai definir o valor de uma propriedade específica
+// let Pessoa2 = {
+//     nome: "Breno",
+//     sobrenome: "Santana",
+//     idioma: "",
+
+//     set linguagem(lang) {
+//         this.idioma = lang;
+//     }
+
+// }
+// Pessoa.linguagem = "Português"
+// console.log(Pessoa.idioma)
+// console.log(Pessoa)
---
+++ js/5_objetos/5_funcoesImportantes.js
@@ -0,0 +1,44 @@
+const pessoa = {
+  nome: 'Rebeca',
+  idade: 2,
+  peso: 13
+}
+
+// Object.keys retorna todas as chaves do objeto em formato de array
+console.log(Object.keys(pessoa));
+
+// Object.values retorna todas os valores do objeto em formato de array
+console.log(Object.values(pessoa)); 
+
+// Object.entries retorna todas as chaves e valores de um objeto em formato de array
+console.log(Object.entries(pessoa));
+
+//
+Object.defineProperty(pessoa, 'dataNascimento', {
+  enumerable: true, // para aparecer nos métodos keys, values, entries, ...
+  writable: false, // funciona como o Object.freeze
+  value: '01/01/2019'
+});
+
+pessoa.dataNascimento = '01/01/2021';
+console.log(pessoa.dataNascimento);
+console.log(Object.keys(pessoa));
+
+/*
+*  Object.assign (ECMAScript 2015)
+*  O primeiro parâmetro é objeto que será acrescido dos elementos dos demais objetos passados como parâmetro
+*/
+
+const dest = { a: 1 };
+console.log('dest', dest);
+const o1 = { b: 2 };
+const o2 = { c: 3, a: 4 };
+const obj = Object.assign(dest, o1, o2);
+console.log('o1', o1);
+console.log('o2', o2);
+console.log('obj', obj);
+
+// Com o Object.freeze o objeto não permitirá qualquer alteração
+Object.freeze(obj);
+obj.c = 1234;
+console.log(obj);
