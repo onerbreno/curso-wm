f55041fb02be48aa2de1887fa6fefaed2976b2e0
Breno <onerbreno@outlook.com>
2023-03-16 08:31:38 -0300

    Capítulo de objeto - aula 130

---
+++ js/5_objetos/9_classe.js
@@ -0,0 +1,52 @@
+class Lancamento {
+    constructor(nome = 'Genérico', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+    addLancamento(...lancamentos) { // rest
+        this.lancamentos.push(...lancamentos) // spread
+        // lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+    sumario() {
+        let valorConsolidado = 0
+        this.lancamentos.forEach(l => {
+            valorConsolidado += l.valor
+        })
+        return valorConsolidado
+    }
+}
+
+const salario = new Lancamento('Salario', 4500)
+const luz = new Lancamento('Luz', -150)
+
+const contas = new CicloFinanceiro(3, 23)
+contas.addLancamento(salario, luz)
+console.log(contas.sumario())
+
+
+class Avo {
+    constructor(sobrenome) {
+        this.sobrenome = sobrenome
+    }
+}
+
+class Pai extends Avo {
+    constructor(sobrenome, profissao = 'Professor') {
+        super(sobrenome)
+        this.profissao = profissao
+    }
+}
+
+class Filho extends Pai {
+    constructor() {
+        super('Silva')
+    }
+}
