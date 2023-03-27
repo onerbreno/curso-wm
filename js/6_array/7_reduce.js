2b119fc1f9986e62969409041a19a0a939a3bc17
Breno <onerbreno@outlook.com>
2023-03-26 23:28:01 -0300

    Capítulo de array JS - aula 143

--- js/6_array/7_reduce.js
+++ js/6_array/7_reduce.js
@@ -5,16 +5,15 @@ const alunos = [
     { nome: 'Ana', nota: 8.7, bolsista: true },
 ]
 
-// const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual) {
-//     console.log(acumulador, atual)
-//     return acumulador + atual
-// }, 0)
-
-// console.log(resultado)
+let somaNotas = alunos.map(e => e.nota).reduce(function(acumulador, atual, i) {
+        console.log(acumulador, atual, i)
+        return acumulador + atual
+    }, 0)
+    
+console.log(somaNotas)
 
 const bolsas = alunos.map(aluno => aluno.bolsista)
-
-
+    
 // Desafio 1: Todos os alunos são bolsistas?
 // const todosBolsistas = bolsas.reduce((acumulador, atual) => {
 //     return acumulador = acumulador ? atual : acumulador
@@ -40,7 +39,36 @@ const bolsistas = e => e.bolsista
 let resultado = alunos.every(bolsistas)
 console.log(resultado)
 
-// some() testa se algum dos elementos do array passa no teste lógico da 
+// some() testa se ALGUM dos elementos do array passa no teste lógico da 
 // função callback e retorna TRUE
 resultado = alunos.some(bolsistas)
 console.log(resultado)
+
+
+// simulando reduce
+Array.prototype.reduce2 = function(callback, initialValue) {
+    // let acumulador = initialValue === undefined ? this[0] : initialValue;
+    // let i = initialValue === undefined ? 1 : 0;
+
+    let acumulador; let i;
+    [acumulador, i] = (typeof initialValue === "undefined") ? [this[0], 1] : [initialValue, 0]
+
+
+    for (i; i < this.length; i++) {
+        acumulador = callback(acumulador, this[i], i, this)
+    }
+
+    return acumulador
+}
+
+let somaNotasReduce2 = alunos.map(e => e.nota).reduce2(function(acumulador, atual, i) {
+    console.log(acumulador, atual, i)
+    return acumulador + atual
+}, 0)
+
+console.log(somaNotasReduce2)
+
+const soma = (total, valor) => total + valor
+const nums = [1, 2, 3, 4, 5, 6]
+
+console.log(nums.reduce2(soma))
