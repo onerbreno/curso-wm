616c44dca4385eadff51d740adfd4a440bbee0fc
Breno <onerbreno@outlook.com>
2023-05-06 00:16:20 -0300

    Capítulo de ESNext - aula 182

---
+++ js/9_esnext/7_set.js
@@ -0,0 +1,17 @@
+// Set() é uma estrutura de dados que não aceita repetição e não é indexada
+const times = new Set()
+times.add('Vasco')
+times.add('São Paulo').add('Palmeiras').add('Coringthians')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('Vasco'))
+console.log(times.has('vasco'))
+times.delete('Flamengo')
+console.log(times.has('Flamengo'))
+
+const nomes = ['Raquel', 'Lucas', 'Julia', 'Lucas']
+const nomesSet = new Set(nomes)
+console.log(nomesSet)
