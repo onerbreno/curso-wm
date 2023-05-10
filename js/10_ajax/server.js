c9026a93d129158889a47fe5bdce71d1c691d45b
Breno <onerbreno@outlook.com>
2023-05-09 23:39:59 -0300

    Capítulo de ajax - aula 307

--- js/10_ajax/server.js
+++ js/10_ajax/server.js
@@ -1,23 +1,30 @@
 const express = require('express')
 const app = express()
 
+// Define o diretório raiz para servir os arquivos estáticos
 app.use(express.static('.'))
+
+// Define um middleware para tratar os dados enviados através de formulários
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+// Importa o módulo multer para lidar com uploads de arquivos
 const multer = require('multer')
 
+// Configura o multer para armazenar os arquivos no diretório './upload'
 const storage = multer.diskStorage({
-    destination: function (req, file, callback) {
+    destination (req, file, callback) {
         callback(null, './upload')
     },
-    filename: function (req, file, callback) {
+    filename (req, file, callback) {
         callback(null, `${Date.now()}_${file.originalname}`)
     }
 })
 
+// Cria um middleware do multer para tratar um único arquivo enviado através do campo "arquivo" do formulário
 const upload = multer({ storage }).single('arquivo')
 
+// Define o endpoint POST "/upload" para receber um arquivo
 app.post('/upload', (req, res) => {
     upload(req, res, err => {
         if (err) {
@@ -28,4 +35,5 @@ app.post('/upload', (req, res) => {
     })
 })
 
-app.listen(8080, () => console.log('Execuntando...'))
+// Inicia o servidor na porta 8080
+app.listen(8080, () => console.log('Executando...'))
